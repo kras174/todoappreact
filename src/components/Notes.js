82180f0b27ce63f9901a4120583c98dd8d1eb0ef
kras174 <kras32a@gmail.com>
2020-07-20 11:36:23 +0500

    stage 2

---
+++ src/components/Notes.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { AlertContext } from "../context/alert/alertContext";
+
+export const Notes = ({ notes, onRemove }) => {
+  const alert = useContext(AlertContext);
+
+  return (
+    <TransitionGroup component="ul" className="list-group">
+      {notes.map(note => (
+        <CSSTransition key={note.id} classNames={"note"} timeout={800}>
+          <li className="list-group-item note">
+            <div>
+              <strong>{note.title}</strong>
+              <small>{note.date}</small>
+            </div>
+            <button
+              type="button"
+              className="btn btn-outline-danger btn-sm"
+              onClick={() => {
+                alert.show("Заметка удалена успешно!", "danger");
+                onRemove(note.id);
+              }}
+            >
+              &times;
+            </button>
+          </li>
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
+  );
+};
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Лучшее приложение в мире</h1>
+      <p className="lead">
+        Версия приложения <strong>1.0.13</strong>
+      </p>
+    </div>
+  </div>
+);
